f3d578cf827944a875572890b64613c952e7a09d
mcmenamc <mcmena636@gmail.com>
2021-12-21 16:08:29 -0600

    version one

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import '@babel/polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
