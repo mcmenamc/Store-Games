a42598194876b1afa5f4b5696e1748b7c7b6a750
mcmenamc <mcmena636@gmail.com>
2022-01-05 18:18:57 -0600

    servidor y cliente conectado

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const cors  = require("cors");
+const path = require("path");
 const app = express();
 // Setting
 app.set("port", process.env.PORT || 3000);
 // Middlewares
 app.use(cors());
 app.use(express.json());
+//public folder
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
 // Routes
 app.use(require("./routes/users")); // ROUTES USERS
 app.use(require("./routes/products")); // ROUTES ARTICLES
+
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
+
 // Starting the server
 app.listen(app.get("port"), () => {
   console.log("Server on port", app.get("port"));
--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mysqlConnection = require('../database/database');
-
+const mysqlConnection = require("../database/database");
 
 // GET all articles
-router.get('/api/products', (req, res) => {
-    mysqlConnection.query('SELECT P.idProducts, P.title, P.description, P.priceSale, TO_BASE64((SELECT MAX(I.image)  FROM image AS I)) AS image FROM products AS P WHERE P.status = 1', (err, rows, fields) => {
-        if (!err) {
+router.get("/api/products", (req, res) => {
+  mysqlConnection.query(
+    "SELECT P.idProducts, P.title, P.description, P.priceSale, TO_BASE64((SELECT MAX(I.image)  FROM image AS I WHERE I.fkProduct = P.idProducts)) AS image FROM products AS P WHERE P.status = 1",
+    (err, rows, fields) => {
+      if (!err) {
         //    console.log(base64_utf8_encode(rows[0]));
-            // res.send("<img src='data:image/*;base64,'"+rows[0]+"'/>");
-            res.json(rows);
-        } else {
-            console.log(err);
-        }
-    });
+        // res.send("<img src='data:image/*;base64,'"+rows[0]+"'/>");
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    }
+  );
 });
-
-
 
 module.exports = router;
